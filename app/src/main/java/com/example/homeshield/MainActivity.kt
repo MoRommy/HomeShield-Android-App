9f7c4cbfee87c7536b58901b5cda81313a5904c7
MoRommy <stefanrgf@yahoo.com>
2024-03-17 19:08:35 +0200

    Added resources for device ui

--- app/src/main/java/com/example/homeshield/MainActivity.kt
+++ app/src/main/java/com/example/homeshield/MainActivity.kt
@@ -3,16 +3,27 @@ package com.example.homeshield
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.homeshield.ui.theme.HomeShieldTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,7 +36,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Device("Poarta", 15921, "Buftea", "locked", true)
+//                    Greeting("HomeShield")
                 }
             }
         }
@@ -33,19 +45,57 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Surface(color = Color.Cyan) {
-        Text(
-            text = "Hello $name!",
-            modifier = modifier.padding(24.dp)
-        )
+fun Device(device: Device, modifier: Modifier = Modifier) {
+    Surface(color = Color.White, modifier = modifier) {
+        Row (
+            modifier = modifier,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(painterResource(id = R.drawable.gate), null, modifier = Modifier
+                .size(64.dp)
+                .padding(15.dp))
+            Column (
+                modifier = modifier.padding(start = 24.dp, end = 128.dp),
+                horizontalAlignment = Alignment.Start
+            ) {
+                Text(
+                    text = device.name,
+                    fontSize = 24.sp,
+                    fontStyle = FontStyle.Normal,
+                    modifier = modifier
+                )
+                Text(
+                    text = device.location,
+                    fontSize = 12.sp,
+                    fontStyle = FontStyle.Italic,
+                    modifier = modifier
+                )
+            }
+            Box (contentAlignment = Alignment.Center) {
+                Button(
+                    onClick = {
+                              device.locked = !device.locked
+                    }, modifier = Modifier.padding(12.dp)
+
+                ) {
+//                    Icon(painterResource(id = R.drawable.lock), contentDescription = null)
+                }
+            }
+        }
     }
 }
+//
+//fun getLockImageVector(device: Device): ImageVector {
+//    if (device.locked)
+//        return
+//    else
+//        return Icons.Rounded.
+//}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     HomeShieldTheme {
-        Greeting("Android")
+        Device(Device("Poarta", 15921, "Buftea", "locked", true))
     }
 }
