0acd6622d25a0f58715e19418753b10723f9072a
MoRommy <stefanrgf@yahoo.com>
2024-04-16 03:34:11 +0300

    added splash screen

--- app/src/main/java/com/example/homeshield/MainActivity.kt
+++ app/src/main/java/com/example/homeshield/MainActivity.kt
@@ -1,22 +1,33 @@
 package com.example.homeshield
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.ComponentActivity
+import androidx.annotation.RequiresApi
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : ComponentActivity() {
     private lateinit var firebaseAuth: FirebaseAuth
 
+    @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
+        splashScreen.setKeepOnScreenCondition{true}
+        Handler(Looper.getMainLooper()).postDelayed({
+            splashScreen.setKeepOnScreenCondition{false}}, 2000)
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         firebaseAuth = FirebaseAuth.getInstance()
-        val user = firebaseAuth.currentUser
 
+        val user = firebaseAuth.currentUser
         if (user == null) {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
